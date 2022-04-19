eccfc1693ea1aba5a4df34f09ce8a03b48152af2
Milo <zeroeclipse@live.com>
2022-04-18 21:21:34 -0700

    Assigning a variable - 2

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,13 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello Tim!");
 
-        int num = 5;
-        System.out.println(num);
+        int myFirstNumber = 2;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int sum = myFirstNumber + mySecondNumber + myThirdNumber;
     //sout === System.out.println()
+        int myLastOne = 1000 - sum;
+        System.out.println(myLastOne);
+
     }
 }
