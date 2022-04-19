3f6cdacfe4190306675b8adcc8ad67dbf7cd7e89
Milo <zeroeclipse@live.com>
2022-04-18 21:07:51 -0700

    Assigning a variable

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,10 @@
 public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello Tim!");
+
+        int num = 5;
+        System.out.println(num);
+    //sout === System.out.println()
+    }
 }
