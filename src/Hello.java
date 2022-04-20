c67c995156754644865c114099ebd0c03e158832
Milo <zeroeclipse@live.com>
2022-04-19 19:28:03 -0700

    Primitive datatypes - 1

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello Tim!");
+//        System.out.println("Hello World!");
 
         int myFirstNumber = 2;
         int mySecondNumber = 12;
@@ -9,7 +9,13 @@ public class Hello {
         int sum = myFirstNumber + mySecondNumber + myThirdNumber;
     //sout === System.out.println()
         int myLastOne = 1000 - sum;
-        System.out.println(myLastOne);
+//        System.out.println(myLastOne);
+
+        int myValue = 10000;
+        int minIntValue = Integer.MIN_VALUE;
+        int maxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + minIntValue);
+        System.out.println("Integer Maximum Value = " + maxIntValue);
 
     }
 }
